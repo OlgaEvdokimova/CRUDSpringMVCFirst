25027982b871f411724f0707cd80db90afdc6586
Olga <olgaaevdokim@gmail.com>
2021-03-21 00:10:08 +0300

    first

---
+++ src/main/java/spring_MVC/controllers/PeopleController.java
@@ -0,0 +1,45 @@
+package spring_MVC.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import spring_MVC.dao.PersonDAO;
+import spring_MVC.models.Person;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping("/list")
+    public String index(Model model) {
+        //Получим всех людей из DAO и передадим на отображение в представление
+       model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        // Получим 1ого чел-ка по id из DAO и передадим на отображение в представление
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model){
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+    @PostMapping
+    public String create(@ModelAttribute("person")Person person){
+        personDAO.save(person);
+        return "redirect:/people/list";
+    }
+}
